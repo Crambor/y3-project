de01183d10084eab82e5a27427312a0a03c925b5
crambor <d.crambor@gmail.com>
2024-03-10 22:04:30 +0000

    chore: move onboot

--- terraform/optiplex2/vms.tf
+++ terraform/optiplex2/vms.tf
@@ -1,3 +1,7 @@
+locals {
+  is_running = true
+}
+
 resource "proxmox_vm_qemu" "slurm-vms" {
   count       = 5
   name        = format("slurm-cn%02d", count.index + 6)
@@ -6,12 +10,12 @@ resource "proxmox_vm_qemu" "slurm-vms" {
   clone       = "template"
   full_clone  = true
   scsihw      = "virtio-scsi-single"
+  agent       = 1
 
   memory  = 2458
   cores   = 1
   os_type = "cloud-init"
   qemu_os = "other"
-  onboot  = false
 
   disks {
     scsi {
@@ -36,8 +40,7 @@ resource "proxmox_vm_qemu" "slurm-vms" {
 
   cloudinit_cdrom_storage = "local-lvm"
 
-  # vm_state = "stopped" # alt is 'running'
-  vm_state = "running"
-
+  vm_state = local.is_running ? "running" : "stopped"
+  onboot   = local.is_running
 }
 
